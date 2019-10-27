c7625517abbf8113faddd35bbb93e60a319f3485
Uzi9 <1711595163@qq.com>
2019-10-27 07:06:38 -0700

    added a file

---
+++ lab1/lab2/bc.c
@@ -0,0 +1,17 @@
+#include<stido.h>
+int main()
+{
+    int i=0;
+    int n1=0;
+    int n2=0;
+    scanf("%d,&n2);
+    while(n2!=0)
+    {
+        n1=n2%2;
+        n2=n2/2;
+        while (n==1)
+        i=i+1;
+        }
+        printf("%d\n",i)
+        return0;
+        }
