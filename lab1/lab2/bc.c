b3e7a76cacf0e265a534f3b22aa3a2354a0015b2
Uzi9 <1711595163@qq.com>
2019-10-31 06:37:13 -0700

    added a bc.c file

--- lab1/lab2/bc.c
+++ lab1/lab2/bc.c
@@ -1,17 +1,17 @@
-#include<stido.h>
+#include<stdio.h>
 int main()
 {
-    int i=0;
-    int n1=0;
-    int n2=0;
-    scanf("%d,&n2);
-    while(n2!=0)
-    {
-        n1=n2%2;
-        n2=n2/2;
-        while (n==1)
-        i=i+1;
-        }
-        printf("%d\n",i)
-        return0;
-        }
+   int i=0;
+   int n1=0;
+   int n2=0;
+   scanf("%d",&n2);
+   while(n2!=0)
+   {
+     n1=n2%2;
+     n2=n2/2;
+     if (n1==1)
+    i=i+1;
+ }
+printf("%d\n",i);
+return 0;
+}
